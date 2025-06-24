519a2abd09482c5ae38dff1bb59e59d0ae660373
Garrett Gonzalez-Rivas <grg@njit.edu>
2025-06-24 18:29:10 -0400

    update filesystem mounts to reflect the new NFS server config

--- modules/filesystems.nix
+++ modules/filesystems.nix
@@ -1,12 +1,14 @@
-{ config, lib, pkgs, ... }:
-
 {
-  
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   services.rpcbind.enable = true;
-  
+
   fileSystems = {
-    "/mnt/project" = {
-      device = "100.85.177.55:/project";
+    "/mnt" = {
+      device = "100.102.164.18:";
       fsType = "nfs";
       options = [
         "x-systemd.automount"
@@ -18,18 +20,19 @@
         "nfsvers=4.2"
       ];
     };
-    "/mnt/scratch" = {
-      device = "100.85.177.55:/scratch";
-      fsType = "nfs";
-      options = [
-        "x-systemd.automount"
-        "x-systemd.idle-timeout=600"
-        "noauto"
-        "sync"
-        "rsize=1048576"
-        "wsize=1048576"
-        "nfsvers=4.2" 
-      ];
-    };
+
+    # "/mnt/scratch" = {
+    #   device = "100.85.177.55:/scratch";
+    #   fsType = "nfs";
+    #   options = [
+    #     "x-systemd.automount"
+    #     "x-systemd.idle-timeout=600"
+    #     "noauto"
+    #     "sync"
+    #     "rsize=1048576"
+    #     "wsize=1048576"
+    #     "nfsvers=4.2"
+    #   ];
+    # };
   };
 }
