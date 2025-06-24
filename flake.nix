2d58bf399822b83a68649716131fd53601c45eeb
Garrett Gonzalez-Rivas <grg@njit.edu>
2025-06-24 19:46:52 -0400

    reminder to setup impermanance for home-manager...

--- flake.nix
+++ flake.nix
@@ -35,6 +35,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # impermanence.url = "github:nix-community/impermanence";
+
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
