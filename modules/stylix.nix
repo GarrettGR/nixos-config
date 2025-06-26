7f4b5608b1b9d29dfc6a972629fdeec9813bcef0
Garrett Gonzalez-Rivas <grg@njit.edu>
2025-06-26 16:23:45 -0400

    add base colorscheme for stylix

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,6 +1,9 @@
 {pkgs, ...}: {
   stylix = {
     enable = true;
+
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+
     cursor = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Ice";
