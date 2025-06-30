0d55dcd726277162120e4b47e33523431a23dc22
Garrett Gonzalez-Rivas <grg@njit.edu>
2025-06-30 18:39:34 -0400

    still working on muvm/fex issues...

--- modules/muvm.nix
+++ modules/muvm.nix
@@ -2,35 +2,37 @@
   pkgs,
   inputs,
   ...
-}: let
-  patchedOverlay = self: super: let
-    originalOverlay = inputs.nixos-muvm-fex.overlays.default self super;
-
-    patchMesaFlags = old: {
-      mesonFlags = let
-        filteredFlags =
-          builtins.filter
-          (flag: !(builtins.match "-Dtools=.*" flag != null))
-          (old.mesonFlags or []);
-
-        filteredFlags2 =
-          builtins.filter
-          (flag: !(builtins.match ".*libgbm-external.*" flag != null))
-          filteredFlags;
-      in
-        filteredFlags2 ++ ["-Dtools=asahi"];
-    };
-  in
-    originalOverlay {
-      mesa-asahi-edge = (originalOverlay.mesa-asahi-edge or super.mesa-asahi-edge).overrideAttrs patchMesaFlags;
-
-      pkgsCross = (originalOverlay.pkgsCross or super.pkgsCross) {
-        gnu64 = (originalOverlay.pkgsCross.gnu64 or super.pkgsCross.gnu64) {
-          mesa-asahi-edge = (originalOverlay.pkgsCross.gnu64.mesa-asahi-edge or super.pkgsCross.gnu64.mesa-asahi-edge).overrideAttrs patchMesaFlags;
+}: {
+  nixpkgs.overlays = [
+    (final: prev: {
+      fex = prev.fex.overrideAttrs (oldAttrs: {
+        doCheck = false;
+        cmakeFlags =
+          oldAttrs.cmakeFlags
+          ++ [
+            (prev.lib.cmakeFeature "JEMALLOC_OVERRIDE_LG_PAGE" "14")
+            (prev.lib.cmakeFeature "JEMALLOC_PAGE_SIZE" "16384")
+          ];
+        # preConfigure = (oldAttrs.preConfigure or "") + ''
+        #   sed -i 's/--with-lg-page=12/--with-lg-page=14/g' $(find . -name "*.cmake")
+        # '';
+      });
+    })
+    (final: prev: {
+      virglrenderer = prev.virglrenderer.overrideAttrs (old: {
+        src = final.fetchurl {
+          url = "https://gitlab.freedesktop.org/asahi/virglrenderer/-/archive/asahi-20250424/virglrenderer-asahi-20250424.tar.bz2";
+          hash = "sha256-9qFOsSv8o6h9nJXtMKksEaFlDP1of/LXsg3LCRL79JM=";
         };
-      };
-    };
-in {
-  nixpkgs.overlays = [patchedOverlay];
-  environment.systemPackages = with pkgs; [muvm];
+        mesonFlags = old.mesonFlags ++ [(final.lib.mesonOption "drm-renderers" "asahi-experimental")];
+      });
+    })
+  ];
+
+  environment.systemPackages = with pkgs; [
+    fex
+    muvm
+
+    libsForQt5.qt5.qtwayland
+  ];
 }
