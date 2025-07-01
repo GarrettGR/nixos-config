7f62ec71983198f064b9123fb92cbe996a06a7b8
Garrett Gonzalez-Rivas <grg@njit.edu>
2025-07-01 18:39:58 -0400

    move to global (system-wide) in flake.nix

--- modules/muvm-overlay.nix
+++ modules/muvm-overlay.nix
@@ -3,15 +3,21 @@
   inputs,
   ...
 }: {
-  environment.systemPackages = let
-    pkgsFex = import pkgs.path {
-      inherit (pkgs) system;
-      config.nixos-muvm-fex.mesaDoCross = true;
-      overlays = [inputs.nixos-muvm-fex.overlays.default];
-    };
-  in [
-    pkgsFex.muvm
-    pkgsFex.fex
-    pkgs.libsForQt5.qt5.qtwayland
-  ];
+  # nixpkgs = {
+  #   config.nixos-muvm-fex.mesaDoCross = true;
+  #   overlays = [inputs.nixos-muvm-fex.overlays.default];
+  # };
+  environment.systemPackages = [pkgs.muvm pkgs.libsForQt5.qt5.qtwayland];
+
+  # environment.systemPackages = let
+  #   pkgsFex = import pkgs.path {
+  #     inherit (pkgs) system;
+  #     config.nixos-muvm-fex.mesaDoCross = true;
+  #     overlays = [inputs.nixos-muvm-fex.overlays.default];
+  #   };
+  # in [
+  #   pkgsFex.muvm
+  #   pkgsFex.fex
+  #   pkgs.libsForQt5.qt5.qtwayland
+  # ];
 }
