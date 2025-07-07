ffa761abceed28b597e2dabbf1a1ed3d4b27b227
Garrett Gonzalez-Rivas <grg@njit.edu>
2025-07-07 12:01:31 -0400

    change the power button behavior (still WIP)

--- hosts/seldon-nix/hardware.nix
+++ hosts/seldon-nix/hardware.nix
@@ -31,6 +31,57 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 
+  services.logind.extraConfig = "HandlePowerKey=ignore";
+
+  systemd.services.power-button = {
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      Type = "simple";
+      Restart = "always";
+      User = "root";
+    };
+    script = ''
+      ${pkgs.evtest}/bin/evtest /dev/input/by-path/platform-macsmc-hid-event |
+      ${pkgs.gnugrep}/bin/grep --line-buffered "KEY_POWER.*value 1" |
+      while read line; do
+        ${pkgs.sudo}/bin/sudo -u garrettgr WAYLAND_DISPLAY=wayland-1 XDG_RUNTIME_DIR=/run/user/1000 ${pkgs.wlogout}/bin/wlogout &
+      done
+    '';
+  };
+
+  security.sudo.extraRules = [
+    {
+      groups = ["wheel"];
+      commands = [
+        {
+          command = "${pkgs.systemd}/bin/systemctl poweroff";
+          options = ["NOPASSWD"];
+        }
+        {
+          command = "${pkgs.systemd}/bin/systemctl reboot";
+          options = ["NOPASSWD"];
+        }
+        {
+          command = "${pkgs.systemd}/bin/systemctl suspend";
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
+
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (action.id == "org.freedesktop.login1.power-off" ||
+        action.id == "org.freedesktop.login1.reboot" ||
+        action.id == "org.freedesktop.login1.suspend" ||
+        action.id == "org.freedesktop.login1.hibernate") {
+          if (subject.isInGroup("wheel")) {
+            return polkit.Result.YES;
+          }
+      }
+    });
+  '';
+
   hardware.graphics.enable = true;
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
