564ba677e830aca53dc9a9b7b4464c251985aa9c
Garrett Gonzalez-Rivas <grg@njit.edu>
2025-07-27 11:09:47 -0400

    change to allowing seldon-nix to connect TODO: update this to allow all machines on my tailscale to be able to connect rather than just seldon-nix... somehow...

--- modules/nfs.nix
+++ modules/nfs.nix
@@ -19,7 +19,7 @@
       enable = true;
       createMountPoints = true;
       exports = ''
-        /srv            *.halley-census.ts.net(insecure,rw,sync,no_root_squash,no_subtree_check,crossmnt,fsid=0)
+        /srv            100.108.152.77(insecure,rw,sync,no_root_squash,no_subtree_check,crossmnt,fsid=0)
         /srv/nfs       	*.halley-census.ts.net(insecure,rw,sync,no_root_squash,no_subtree_check,nohide)
         # /nfs/scratch	10.0.0.0/24(insecure,rw,sync,no_root_squash,no_subtree_check,nohide)
       '';
