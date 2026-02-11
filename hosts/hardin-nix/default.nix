89984f877e560b9880ee2c224360a26387206eac
Garrett Gonzalez-Rivas <grg@njit.edu>
2026-02-11 03:32:00 -0500

    apply a patch to 6.18.8 asahi kernel so it'll build

--- hosts/hardin-nix/default.nix
+++ hosts/hardin-nix/default.nix
@@ -14,17 +14,43 @@
 
   networking.hostName = "${hostname}";
 
-  boot.loader.grub = {
-    enable = true;
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-    device = "nodev";
-    gfxmodeEfi = "2560x1664";
+  boot = {
+    kernelPatches = [
+      {
+        name = "fix-asahi-build";
+        patch = pkgs.writeText "fix-asahi-build.diff" ''
+          diff --git a/rust/kernel/iio/common/mod.rs b/rust/kernel/iio/common/mod.rs
+          index 570644ce0938..b789e9bf44c9 100644
+          --- a/rust/kernel/iio/common/mod.rs
+          +++ b/rust/kernel/iio/common/mod.rs
+          @@ -2,10 +2,5 @@
+
+           //! IIO common modules
+
+          -#[cfg(any(
+          -    CONFIG_IIO_AOP_SENSOR_LAS = "y",
+          -    CONFIG_IIO_AOP_SENSOR_ALS = "m",
+          -    CONFIG_IIO_AOP_SENSOR_LAS = "y",
+          -    CONFIG_IIO_AOP_SENSOR_ALS = "m",
+          -))]
+          +#[cfg(any(CONFIG_IIO_AOP_SENSOR_LAS, CONFIG_IIO_AOP_SENSOR_ALS,))]
+           pub mod aop_sensors;
+        '';
+      }
+    ];
+    loader.grub = {
+      enable = true;
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      device = "nodev";
+      gfxmodeEfi = "2560x1664";
+    };
   };
 
   hardware.asahi.peripheralFirmwareDirectory = /etc/nixos/firmware;
 
   environment.systemPackages = with pkgs; [
+    asahi-bless
     asahi-btsync
     asahi-wifisync
     # asahi-nvram
