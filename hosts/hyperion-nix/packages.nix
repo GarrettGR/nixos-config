2367dc6a176ee600525c6e82b6734cec7c28d33f
Garrett Gonzalez-Rivas <grg@njit.edu>
2026-02-24 02:22:19 -0500

    allow X11 forwarding via SSH

--- hosts/hyperion-nix/packages.nix
+++ hosts/hyperion-nix/packages.nix
@@ -7,7 +7,14 @@
 }: let
   pkgs-stable = inputs.nixpkgs-stable.legacyPackages.${pkgs.stdenv.hostPlatform.system};
 in {
-  environment.systemPackages = with pkgs; [];
+  environment.systemPackages = with pkgs; [
+    xauth
+  ];
+
+  services.openssh = lib.mkForce {
+    enable = true;
+    settings.X11Forwarding = true;
+  };
 
   services.desktopManager.plasma6.enable = true;
 
