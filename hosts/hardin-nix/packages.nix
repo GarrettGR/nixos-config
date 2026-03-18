58dff26d2404e1ffdd56a6b2a3085402d7ee1b52
Garrett Gonzalez-Rivas <grg@njit.edu>
2026-03-18 13:24:59 -0400

    add KDE Plasma 6 as a fallback due to recent hyprland instability

--- hosts/hardin-nix/packages.nix
+++ hosts/hardin-nix/packages.nix
@@ -8,6 +8,7 @@
   pkgs-stable = inputs.nixpkgs-stable.legacyPackages.${pkgs.stdenv.hostPlatform.system};
 in {
   services.containers.enableDocker = true;
+  services.desktopManager.plasma6.enable = true;
   environment.systemPackages = with pkgs; [
     moonlight-embedded
     distrobox
