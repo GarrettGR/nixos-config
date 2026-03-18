07e0bb506910ae71c185d182538787d58288ca03
Garrett Gonzalez-Rivas <grg@njit.edu>
2026-03-18 13:14:35 -0400

    add steam to hyperion-nix

--- hosts/hyperion-nix/packages.nix
+++ hosts/hyperion-nix/packages.nix
@@ -25,4 +25,12 @@ in {
     autoStart = true;
     # applications = {};
   };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    extraCompatPackages = with pkgs; [proton-ge-bin];
+  };
 }
