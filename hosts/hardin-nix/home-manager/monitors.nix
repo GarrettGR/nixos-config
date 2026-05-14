e0e5e257877d62ea43c58c84879a1ac87efe6de8
Garrett Gonzalez-Rivas <grg@njit.edu>
2026-05-14 16:20:10 -0400

    add the config for the large monitors in the SFS room

--- hosts/hardin-nix/home-manager/monitors.nix
+++ hosts/hardin-nix/home-manager/monitors.nix
@@ -2,6 +2,7 @@
   wayland.windowManager.hyprland.settings.monitor = lib.mkBefore [
     "desc:Samsung Electric Company SAMSUNG, 3840x2160@60, 0x0, 1.5, cm, auto"
     "desc:RGB Systems Inc. dba Extron Electronics Extron HDMI, 1920x1080@60, 4072x1485, 1"
+    "desc:LG Electronics LG ULTRAWIDE 401NTBK36869, 3440x1440@60, 4072x0, 1"
     "eDP-1, 3024x1964@120, 2560x601, 2, cm, auto"
   ];
 }
