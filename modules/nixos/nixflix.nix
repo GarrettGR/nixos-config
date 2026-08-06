9b917eeee33463f04a6b3e835776df05b5c6413a
Garrett Gonzalez-Rivas <grg@njit.edu>
2026-08-06 10:18:08 -0400

    update nixflix config and enable additional services
    - enable seer, radarr, prowlarr, and sonarr (with postgress integration)
    - reconfigure the nginx proxy (simplify using `serverAliases`)

--- modules/nixos/nixflix.nix
+++ modules/nixos/nixflix.nix
@@ -1,11 +1,13 @@
-# Media server (jellyfin + qbittorrent via nixflix). Self-imports the upstream
-# nixflix module.
 {
   inputs,
   config,
   ...
 }: {
-  flake.modules.nixos.nixflix = {pkgs, ...}: {
+  flake.modules.nixos.nixflix = {
+    pkgs,
+    lib,
+    ...
+  }: {
     imports = [inputs.nixflix.nixosModules.default];
 
     nixflix = {
@@ -14,16 +16,24 @@
       downloadsDir = "/data/downloads";
       mediaDir = "/data/media";
       stateDir = "/data/.state";
+
       nginx = {
         enable = true;
+        domain = "hyperion-nix";
         addHostsEntries = true;
       };
+
+      postgres.enable = true;
+
       torrentClients.qbittorrent = {
         enable = true;
         serverConfig.Preferences.WebUI = {
           Username = config.flake.user.name;
         };
       };
+
+      downloadarr.enable = true;
+
       jellyfin = {
         enable = true;
         encoding = {
@@ -36,30 +46,7 @@
         };
         network = {
           autoDiscovery = true;
-          enableUPnP = true; # insecure ??
-          knownProxies = ["127.0.0.1"];
-        };
-        libraries = {
-          Movies = {
-            collectionType = "movies";
-            enableRealtimeMonitor = true;
-            metadataCountryCode = "US";
-            paths = [
-              "/data/media/movies"
-              "/mnt/drive/media/movies"
-            ];
-            preferredMetadataLanguage = "en";
-          };
-          Shows = {
-            collectionType = "tvshows";
-            paths = [
-              "/data/media/tv"
-              "/data/media/anime"
-              "/mnt/drive/media/tv"
-              "/mnt/drive/media/anime"
-            ];
-            seasonZeroDisplayName = "Specials";
-          };
+          enableUPnP = true;
         };
         users = {
           ${config.flake.user.name} = {
@@ -69,40 +56,74 @@
           shikhar = {};
           corinne = {};
         };
+
+        plugins.AniDB.enable = false; # breaking on `builtins.convertHash` (missing on lix version)
+      };
+
+      sonarr = {
+        enable = true;
+        mediaDirs = [
+          "/data/media/tv"
+          "/mnt/drive/media/tv"
+        ];
+      };
+
+      sonarr-anime = {
+        enable = true;
+        mediaDirs = [
+          "/data/media/anime"
+          "/mnt/drive/media/anime"
+        ];
+      };
+
+      radarr = {
+        enable = true;
+        mediaDirs = [
+          "/data/media/movies"
+          "/mnt/drive/media/movies"
+        ];
+      };
+
+      prowlarr = {
+        enable = true;
+      };
+
+      seerr = {
+        enable = true;
+        externalUrlScheme = "http";
+      };
+
+      recyclarr = {
+        enable = true;
+        radarrQuality = "1080p";
+        sonarrQuality = "1080p";
       };
     };
 
-    services.nginx.virtualHosts = let
-      jellyfinLocations = {
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8096";
-          recommendedProxySettings = true;
-          extraConfig = ''
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_buffering off;
-          '';
-        };
-        locations."/socket" = {
-          proxyPass = "http://127.0.0.1:8096";
-          proxyWebsockets = true;
-          recommendedProxySettings = true;
-          extraConfig = ''
-            proxy_set_header X-Real-IP $remote_addr;
-          '';
-        };
+    services.nginx.virtualHosts."jellyfin.garrettgr.net" = {
+      serverAliases = [
+        "hyperion-nix"
+        "hyperion-nix.halley-census.ts.net"
+        "100.98.175.99"
+      ];
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8096";
+        recommendedProxySettings = true;
+        extraConfig = ''
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_buffering off;
+        '';
       };
-    in {
-      "hyperion-nix" =
-        jellyfinLocations
-        // {
-          serverAliases = ["hyperion-nix.halley-census.ts.net"];
-        };
-      "100.98.175.99" = jellyfinLocations;
-      "jellyfin.garrettgr.net" = jellyfinLocations;
+      locations."/socket" = {
+        proxyPass = "http://127.0.0.1:8096";
+        proxyWebsockets = true;
+        recommendedProxySettings = true;
+        extraConfig = ''
+          proxy_set_header X-Real-IP $remote_addr;
+        '';
+      };
     };
 
-    environment.systemPackages = with pkgs; [
-      qbittorrent
-    ];
+    environment.systemPackages = with pkgs; [qbittorrent];
   };
 }
